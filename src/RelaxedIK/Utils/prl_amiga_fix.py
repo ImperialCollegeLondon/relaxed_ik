8190d0389e9dbe7214a851b6536c7a7ef8f42221
Cedric Goubard <c.goubard21@imperial.ac.uk>
2022-07-25 14:58:36 +0100

    WIP fixing amiga

--- src/RelaxedIK/Utils/prl_amiga_fix.py
+++ src/RelaxedIK/Utils/prl_amiga_fix.py
@@ -6,11 +6,11 @@ CYLINDER_SHAPES = {
     0: (1.5, 0.038 * 5),  # base
     1: (1.5, 0.178 * 1.2),  # shoulder
     2: (1.5, 0.613 * 1.3),  # upper arm
-    3: (1.4, 0.571 * 1.2),  # forearm
+    3: (1.3, 0.571 * 1.1),  # forearm
     4: (1.4, 0.12  * 1.4),  # wrist 1
     5: (1.4, 0.12  * 1.4),  # wrist 2
-    6: (0.9, 0.05  * 1.4),  # wrist 3
-    7: (1.5, 0.15),  # ?
+    6: (0.9, 0.05  * 1.7),  # wrist 3
+    7: (1.5, 0.28),  # ?
 }
 
 def adapt_cylinder_shapes_to_amiga(link_id: int) -> Tuple[float, float]:
@@ -18,14 +18,22 @@ def adapt_cylinder_shapes_to_amiga(link_id: int) -> Tuple[float, float]:
 
 def fix_tansforms_amiga(ptA, ptB, link_id, all_frames):
     ptA, ptB = copy.deepcopy(ptA), copy.deepcopy(ptB)
+    link67_offset = [-0.117, 0.117 ,0]
 
     # Fix elbow offset defined in ur10e urdf
     if link_id == 2:
         rot = all_frames[0][1][3]
         ptB = ptB + rot.dot([0, 0.137 ,0])
 
-    if link_id == 6:
+    elif link_id == 6:
         rot = all_frames[0][1][7]
-        ptB = ptB + rot.dot([-0.117, 0.117 ,0])
+        ptB = ptB + rot.dot(link67_offset)
+
+    elif link_id == 7:
+        rot = all_frames[0][1][7]
+        ptA = ptA + rot.dot(link67_offset)
+        
+        rot = all_frames[0][1][8]
+        ptB = ptB + rot.dot([-0.22, 0.23, -0.23])
 
     return ptA, ptB
