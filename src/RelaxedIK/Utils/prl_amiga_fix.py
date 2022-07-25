8c765c274b9db00142f5c64093e4a066f7e277ca
Cedric Goubard <c.goubard21@imperial.ac.uk>
2022-07-25 13:07:13 +0100

    WIP fixing amiga

--- src/RelaxedIK/Utils/prl_amiga_fix.py
+++ src/RelaxedIK/Utils/prl_amiga_fix.py
@@ -2,14 +2,14 @@ from typing import Tuple
 import copy
 
 CYLINDER_SHAPES = {
-       # radius factor, length
-    0: (1.5, 0.038),  # base
-    1: (1.5, 0.178),  # shoulder
-    2: (1.5, 0.613),  # upper arm
-    3: (1.5, 0.571),  # forearm
-    4: (1.5, 0.12),  # wrist 1
-    5: (1.5, 0.12),  # wrist 2
-    6: (0.9, 0.05),  # wrist 3
+       # radius factor, urdf length * adjustment factor
+    0: (1.5, 0.038 * 5),  # base
+    1: (1.5, 0.178 * 1.2),  # shoulder
+    2: (1.5, 0.613 * 1.3),  # upper arm
+    3: (1.4, 0.571 * 1.2),  # forearm
+    4: (1.4, 0.12  * 1.4),  # wrist 1
+    5: (1.4, 0.12  * 1.4),  # wrist 2
+    6: (0.9, 0.05  * 1.4),  # wrist 3
     7: (1.5, 0.15),  # ?
 }
 
@@ -23,5 +23,9 @@ def fix_tansforms_amiga(ptA, ptB, link_id, all_frames):
     if link_id == 2:
         rot = all_frames[0][1][3]
         ptB = ptB + rot.dot([0, 0.137 ,0])
+
+    if link_id == 6:
+        rot = all_frames[0][1][7]
+        ptB = ptB + rot.dot([-0.117, 0.117 ,0])
 
     return ptA, ptB
